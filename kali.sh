25a81c98202b17c55e90b5daee54b9d081ce4265
ArieSR91 <94450554+ArieSR91@users.noreply.github.com>
2022-07-21 12:55:28 +0800

    Add files via upload

---
+++ kali.sh
@@ -0,0 +1,13 @@
+# This is a default distribution plug-in.
+# Do not modify this file as your changes will be overwritten on next update.
+# If you want customize installation, please make a copy.
+DISTRO_NAME="Kali (Rolling)"
+DISTRO_COMMENT="Only for Aarch64 hosts."
+
+TARBALL_URL['aarch64']="https://github.com/ArieSR91/install-debian/releases/download/kali-linux/kali.tar.xz"
+TARBALL_SHA256['aarch64']="89cf6de9c0e93cbdd08782a9241df7fac01dc47ff39681866a63b093dca1900d"
+
+distro_setup() {
+	# Don't update gvfs-daemons and udisks2
+	run_proot_cmd apt-mark hold udisks2
+}

d22702af7828bf2e62e89087a68793dda83bf498
ArieSR91 <94450554+ArieSR91@users.noreply.github.com>
2022-11-25 10:54:04 +0800

    Update kali.sh

--- kali.sh
+++ kali.sh
@@ -6,6 +6,8 @@ DISTRO_COMMENT="Only for arm devices (32 & 64)."
 
 TARBALL_URL['aarch64']="https://github.com/ArieSR91/proot-distro-kali/releases/download/kali-linux/kali-arm64.tar.xz"
 TARBALL_SHA256['aarch64']="89cf6de9c0e93cbdd08782a9241df7fac01dc47ff39681866a63b093dca1900d"
+TARBALL_URL['arm']=""
+TARBALL_SHA256['arm']=""
 
 distro_setup() {
 	# Don't update udisks2

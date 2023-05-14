5a13431f20e709805590d01a9b2b87463b5ec6fd
ArieSR91 <94450554+ArieSR91@users.noreply.github.com>
2023-05-15 06:47:54 +0800

    Update kali.sh

--- kali.sh
+++ kali.sh
@@ -1,6 +1,6 @@
 # Kali Linux proot-distro by Arie-SR91
 DISTRO_NAME="Kali (Rolling)"
-DISTRO_COMMENT="Only for arm devices (32 & 64)."
+DISTRO_COMMENT="Currently available only AArch64 and ARM ports"
 
 TARBALL_URL['aarch64']="https://github.com/ArieSR91/proot-distro-kali/releases/download/kali-linux64/kali-arm64_1.0.tar.xz"
 TARBALL_SHA256['aarch64']="89cf6de9c0e93cbdd08782a9241df7fac01dc47ff39681866a63b093dca1900d"
